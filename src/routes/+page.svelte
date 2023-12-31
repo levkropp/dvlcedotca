5da260ac12fed924ff00b4046b16b5bebbfaac6a
Lev Kropp <levkropp@protonmail.com>
2023-12-31 17:00:05 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="">
+	<nav class="bg-black bg-opacity-70 backdrop-filter backdrop-blur-lg fixed top-0 left-0 right-0 z-50">
+		<div class="container mx-auto px-4 sm:px-6 py-3 flex flex-wrap justify-between items-center">
+		  <div class="flex justify-around items-center w-full md:w-auto">
+			<a href="/" class="text-white text-lg font-semibold hover:text-gray-300 transition-colors duration-300">
+			  DVLCE.ca
+			</a>
+		  </div>
+		  <!-- Responsive Toggle -->
+		  <div class="w-full md:hidden flex justify-center mt-3">
+			<a href="#home" class="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-white hover:bg-opacity-10 hover:text-gray-300 transition duration-300 ease-in-out">
+			  Home
+			</a>
+			<a href="#about" class="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-white hover:bg-opacity-10 hover:text-gray-300 transition duration-300 ease-in-out">
+			  About
+			</a>
+			<a href="#contact" class="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-white hover:bg-opacity-10 hover:text-gray-300 transition duration-300 ease-in-out">
+			  Contact
+			</a>
+		  </div>
+		  <!-- Desktop Menu -->
+		  <div class="hidden md:flex items-center space-x-3">
+			<a href="#home" class="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-white hover:bg-opacity-10 hover:text-gray-300 transition duration-300 ease-in-out">
+			  Home
+			</a>
+			<a href="#about" class="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-white hover:bg-opacity-10 hover:text-gray-300 transition duration-300 ease-in-out">
+			  About
+			</a>
+			<a href="#contact" class="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-white hover:bg-opacity-10 hover:text-gray-300 transition duration-300 ease-in-out">
+			  Contact
+			</a>
+		  </div>
+		</div>
+	  </nav>
+
+
+</div>
+
+<style>
+
+
+</style>
